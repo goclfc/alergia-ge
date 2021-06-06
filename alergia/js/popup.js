a41e47582c4c1b7b675a89e1b0064f30ece326e7
goclfc <61660956+goclfc@users.noreply.github.com>
2021-06-06 22:00:36 +0400

    upload
    ad

---
+++ alergia/js/popup.js
@@ -0,0 +1,8 @@
+// let popup=document.getElementById("popup")
+// popup.addEventListener("click", function() {
+// popup.style.width="100vw"
+// popup.style.height="auto"
+// popup.style.position="relative"
+// popup.style.zIndex="999"
+
+//  });
